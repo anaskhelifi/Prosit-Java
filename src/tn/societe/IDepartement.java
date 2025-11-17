a0100f5f811c8245a8902bdcc6e87ff40908f3e7
anaskhelifi <126191973+anaskhelifi@users.noreply.github.com>
2025-11-17 11:25:47 +0100

    Delete src/tn/societe directory

--- src/tn/societe/IDepartement.java
+++
@@ -1,9 +0,0 @@
-package tn.societe;
-
-public interface IDepartement<T> {
-    void ajouterDepartement(T t);
-    boolean rechercherDepartement(String nom);
-    boolean rechercherDepartement(T t);
-    void supprimerDepartement(T t);
-    void displayDepartement();
-}
