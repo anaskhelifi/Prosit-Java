3b1be5016c7e64e1d2864f207f1ad3a27c993d7f
anaskhelifi <126191973+anaskhelifi@users.noreply.github.com>
2025-11-24 11:53:08 +0100

    Add files via upload

---
+++ src/tn/societe/DepartementHashSet.java
@@ -0,0 +1,50 @@
+package tn.societe;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class DepartementHashSet implements IDepartement<Departement> {
+
+    private HashSet<Departement> liste = new HashSet<>();
+
+    @Override
+    public void ajouterDepartement(Departement d) {
+        liste.add(d);
+    }
+
+    @Override
+    public boolean rechercherDepartement(String nom) {
+        for (Departement d : liste) {
+            if (d.getNomDepartement().equalsIgnoreCase(nom))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean rechercherDepartement(Departement d) {
+        return liste.contains(d);
+    }
+
+    @Override
+    public void supprimerDepartement(Departement d) {
+        liste.remove(d);
+    }
+
+    @Override
+    public void displayDepartement() {
+        for (Departement d : liste) {
+            System.out.println(d);
+        }
+    }
+
+    @Override
+    public TreeSet<Departement> trierDepartementById() {
+        TreeSet<Departement> sorted = new TreeSet<>((d1, d2) -> d1.getId() - d2.getId());
+        sorted.addAll(liste);
+        return sorted;
+    }
+
+}
+
+
